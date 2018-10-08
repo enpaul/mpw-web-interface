812bc2d68f0ed28074d51c9c44de2a84bf83bd59
Ethan N. Paul <enpaul@vdln.foundatio.nl>
2018-10-07 23:29:05 -0400

    Add advanced options show/hide toggle

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+$("#toggle-advanced").click(function () {
+  if ($('#toggle-advanced').is(':checked')) {
+    $('#advanced').removeClass('hide');
+    $('#toggle-advanced-label').html("Hide advanced options");
+  }
+  else {
+    $('#advanced').addClass('hide');
+    $('#toggle-advanced-label').html("Show advanced options");
+  }
+});
