cbef7804fbb24f25a1ad564ee253db3f2e18f6b5
Ethan N. Paul <enpaul@vdln.foundatio.nl>
2018-10-08 00:11:47 -0400

    Implement basic version of mpw calc function

--- js/app.js
+++ js/app.js
@@ -10,3 +10,23 @@ $("#toggle-advanced").click(function () {
     $('#toggle-advanced-label').html("Show advanced options");
   }
 });
+
+
+$("#generate").click(function () {
+
+  name = $('#mpw-name').val();
+  site = $('#mpw-site').val();
+  pass = $('#mpw-masterpass').val();
+  ver = $('#mpw-version').val();
+  count = $('#mpw-counter').val();
+  alg = $('#mpw-algorithm').val();
+
+  mpw = new MPW(name, pass, ver);
+  promise = mpw.generate(site, count, "", alg);
+
+  promise.then(function (pass) {
+    $('#output-dispaly').removeClass('hide');
+    $('#mpw-output').val(pass);
+  });
+
+});
